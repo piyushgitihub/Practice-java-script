fabd0cb3f667488d4f1a7dde4720f4c983a6c834
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-05 02:53:03 +0000

    conversion of datatypes

---
+++ Basic/conversionOrOperation.js
@@ -0,0 +1,27 @@
+let score ="43"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let numberValue = Number(score);
+// console.log(numberValue);
+// console.log(typeof numberValue)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "json"
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "name" => true
+
+let someNumber = 18
+
+let stringsomeNumber = String(someNumber);
+console.log(stringsomeNumber);
+console.log(typeof stringsomeNumber);
