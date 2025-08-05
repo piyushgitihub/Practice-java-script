86b9ad071d6c39d94abefa01a06933619941cbe6
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-05 13:56:05 +0000

    operation of numbers

--- Basic/conversionOrOperation.js
+++ Basic/conversionOrOperation.js
@@ -23,5 +23,33 @@ let booleanisLoggedIn = Boolean(isLoggedIn);
 let someNumber = 18
 
 let stringsomeNumber = String(someNumber);
-console.log(stringsomeNumber);
-console.log(typeof stringsomeNumber);
+// console.log(stringsomeNumber);
+// console.log(typeof stringsomeNumber);
+
+// **********OPERATIONS***********
+
+let value = 5
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%4);    
+
+// console.log(2+"2");
+// console.log(1+ 2+ "2");
+// console.log("1" + 2 + 2);
+
+// console.log(true);
+// console.log(+"");
+
+let num = 3;
+const pre = num++;
+// const post = ++num;
+//  console.log(pre);
+//  console.log(num);
+
