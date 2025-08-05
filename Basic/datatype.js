33d9d5b744120d74ba3c35cd9c590dd024eefa85
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-05 02:22:53 +0000

    datatypes

---
+++ Basic/datatype.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version
+
+//alert(6 + 9) // we are using node.js, not browser
+console.log(3 
+    + 3);
+// code should be readable by the user and programmer i.e: readability
+
+console.log("milu")
+
+//whenever you try to declare a variable , use LET instead of VAR
+let name = "piyush"
+let age = 18
+let isLoggedIn = false
+let State; // Undefined
+let states = null; // Here we intentionally put null(EMPTY)
+
+// number => 2 to power 53
+// bigint
+// String => " "
+// boolean => true/false
+// null => standalone Value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof undefined); // type
+console.log(typeof null); // object
---
+++ Basic/variable.js
@@ -0,0 +1,20 @@
+const accountId = 123232
+let accounPassword = "234565"
+var acccountEmail = "diddy@gmail.com"
+accountAddress = "Balasore"
+
+// accountId = "687869"
+
+// We can not change the constant value
+
+
+accounPassword = "798657"
+acccountEmail = "juston@gmail.com"
+accountAddress = "Bhubaneswar"
+
+/*
+Prefer not to use var
+because of issues in block scope and functional scope
+*/
+
+console.table([accountId, accounPassword, acccountEmail, accountAddress]);
