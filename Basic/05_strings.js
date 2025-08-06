841bd380aa195cd9fa371f97233ca5774bd83c6f
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-06 11:25:54 +0000

    string and method

---
+++ Basic/05_strings.js
@@ -0,0 +1,21 @@
+const name = "piyush"
+const repoCount = 50
+
+// console.log(name + repoCount + " value ");
+
+console.log(`Hello my name is${name} and my repo count is${repoCount}`);
+
+const gameName = new String('piyushhc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0, 5)
+console.log(newString);
+
+// const anotherString = 
