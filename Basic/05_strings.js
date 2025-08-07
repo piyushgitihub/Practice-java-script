6c5afaa4c5a5d8a053a25a01a004e229af548d6b
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-07 14:28:56 +0000

    string representation

--- Basic/05_strings.js
+++ Basic/05_strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is${name} and my repo count is${repoCount}`);
 
-const gameName = new String('piyushhc')
+const gameName = new String('piyush-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -16,6 +16,21 @@ const gameName = new String('piyushhc')
 // console.log(gameName.indexOf('s'));
 
 const newString = gameName.substring(0, 5)
-console.log(newString);
+// console.log(newString);
 
-// const anotherString = 
+
+const anotherString = gameName.slice(0, 3)
+// console.log(anotherString);
+
+
+const newString1 = "  piyush  "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+url = "https://piyush.com/piyush%30choudhary"
+
+console.log(url.replace('%30', '_'))
+
+console.log(url.includes('piyush'))
+
+console.log(gameName.split('-'))
