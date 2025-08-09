02f34cc7ac6b4a9871bb1b86e642f7b5a692b186
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-09 15:31:18 +0000

    arrays

---
+++ 02_Basic/01_arrays.js
@@ -0,0 +1,40 @@
+// array
+
+const myArr = [1, 2, 3, 4, 5, 6]
+
+
+// console.log(myArr[0]);
+
+// Arrays methods
+
+// myArr.push(9)
+// myArr.push(69)
+
+// myArr.pop(69)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+
+
+// console.log(myArr)
+// console.log(newArr)
+
+// console.log(typeof newArr)
+
+// slice, splice
+console.log("A ",myArr)
+
+const mna1 = myArr.slice(1, 3)
+console.log(mna1)
+
+console.log("B", myArr)
+
+const mna2 = myArr.splice(1, 3)
+console.log(mna2)
+
+console.log("C ", myArr)
