28c37a8c49ae002fdc3643dd151f86c29676468d
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-11 06:47:05 +0000

    object2

---
+++ 02_Basic/04_objecs.js
@@ -0,0 +1,14 @@
+// 
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "lil@gmail.com",
+    
+}
