685dd12e29896162d65b79c1c7f73175e0beae81
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-12 15:21:21 +0000

    object operatiion

--- 02_Basic/04_objecs.js
+++ 02_Basic/04_objecs.js
@@ -1,4 +1,8 @@
-// 
+// singleton 
+
+// const tinder = new object() 
+
+// non-singleton
 
 const tinderUser = {}
 
@@ -10,5 +14,52 @@ tinderUser.isLoggedIn = false
 
 const regularUser = {
     email: "lil@gmail.com",
-    
+    fullname: {
+        userfullname: {
+            firstname: "hitesh"
+        }
+    }
 }
+
+// console.log(regularUser.fullname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// SPREAD oper
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "lol@gmail.com"
+    },
+    {
+        id: 1,
+        email: "lol@gmail.com"
+    },
+    {
+        id: 1,
+        email: "lol@gmail.com"
+    },{
+        id: 1,
+        email: "lol@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
