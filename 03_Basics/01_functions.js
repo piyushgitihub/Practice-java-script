4c6e56784426171299272417084424ffbe5cfe02
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-15 17:05:25 +0000

    function with parameter

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -39,4 +39,33 @@ function loginUserMessage(username){
 }
 //console.log(loginUserMessage("piyush"))
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 600, 3000))
+
+const user = {
+    username: "piyush",
+    price: 4000
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and Price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 300, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 300, 800]))
