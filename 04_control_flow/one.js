377b435c27dadabd265bb738bcaac7b03ac57909
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-20 19:09:45 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,21 @@
+// if
+
+// const isLoggedIn = true
+// const temperature = 38
+
+// if ( temperature <= 40){
+//     console.log("today it feels like cold")
+// } else {
+//     console.log("today it feels like warm")
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fire"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
