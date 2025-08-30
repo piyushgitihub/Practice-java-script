a7a296963b1b88c37bd4ec9ccd8314168609065c
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-08-30 18:23:09 +0000

    practice

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,24 +1,24 @@
 // if
 
 // const isLoggedIn = true
-const temperature = 38
+// const temperature = 38
 
-if ( temperature <= 40){
-    console.log("today it feels like cold")
-} else {
-    console.log("today it feels like warm")
-}
+// if ( temperature <= 40){
+//     console.log("today it feels like cold")
+// } else {
+//     console.log("today it feels like warm")
+// }
 
 // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+const score = 200
 
-// if(score > 100){
-//     const power = "fire"
-//     console.log(`User power: ${power}`);
-// }
+if(score > 100){
+    const power = "fire"
+    console.log(`User power: ${power}`);
+}
 
-// console.log(`User power: ${power}`);
+// console.log(`User power: ${power}`); // it is the wrong command
 
 // const balance = 1000
 
--- PRACTICE/one.js
+++ PRACTICE/one.js
@@ -1,19 +1,19 @@
 // ARRAY " an empty array"
 
-const a = []
-console.log(a)
+// const a = []
+// console.log(a)
 
 // initialization of array
 
-const b = [10, 20, 30, 40]
-console.log(b)
+// const b = [10, 20, 30, 40]
+// console.log(b)
 
 
 // 
 
-const score = 10
+const num = 101
 
-if(score <= 100){
+if(num <= 100){
     console.log("Number")
 } else{
     console.log("Big Number")
