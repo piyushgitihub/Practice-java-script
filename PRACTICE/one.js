073fc2f594b7dcf0cd081dec76ea643e9f5b2425
piyushgitihub <164828231+piyushgitihub@users.noreply.github.com>
2025-09-03 17:58:54 +0000

    array operation

--- PRACTICE/one.js
+++ PRACTICE/one.js
@@ -33,8 +33,37 @@
 
 // console.log(mul(8, 9))
 
-let x = 20;
-let y = 50;
-let z = x + y;
+// let x = 20;
+// let y = 50;
+// let z = x + y;
 
-console.log(z)
+// console.log(z)
+
+// accessing last element of the array
+
+// const myarr = ["HTML", "CSS", "JS", "PY"]
+
+// let last = myarr[myarr.length - 1]
+
+// console.log("Last Element : " , last)
+
+// Modify the array element
+
+// const hero = ["IRON MAN", "FLASH", "THOR", "SUPER MAN"]
+// console.log(hero);
+
+// hero[1] = "HULK";
+// console.log(hero);
+
+// Adding element to the array 
+
+let num = [10, 20, 30 ,40]
+
+// adding element to the end of the array
+num.push(50);
+
+// adding element to the beginning of the array
+num.unshift(0);
+
+console.log(num)
+
